cbd7a7705732e6878a47e24c52bbaf5697ea85fb
oscarrial <oscarrial121@gmail.com>
2025-02-06 10:50:56 +0100

    v6

--- src/routes/offert.routes.ts
+++ src/routes/offert.routes.ts
@@ -1,24 +1,29 @@
 import { Router } from "express";
-import {AuthController} from '../controllers/auth.controller'
 import { loginValidation, registerValidation } from "../middlewares/validators.middleware";
 import { ValidationMiddleware } from "../middlewares/validation.middleware";
+import { OfferController } from "../controllers/offer.controller";
+import { isAuthenticate } from "@/middlewares/auth.middleware";
 const router = Router()
 
-/* 
-//GET Listar todas las ofertas localhost:3000/api/offerts/?title=react&category=dam
-router.get('/', OffertController.getAll)
-//POST añadir una oferta nueva localhost:3000/api/offerts/  {body}
-router.post('/', OffertController.save)
-//DELETE Borrar una oferta localhost:3000/api/offerts/XXXX  
-router.delete('/:id', OffertController.delete)
-//PUT modificar una oferta localhost:3000/api/offerts/XXXX  {body}
-router.put('/:id', OffertController.update)   
+//API REST FULL
+
+
+//GET Listar todas las ofertas localhost:3000/api/offers/?title=react&category=dam
+router.get('/', OfferController.getAll)
+//localhost:3000/api/offers/xxxx
+router.get('/:id', OfferController.getById)
+//POST añadir una oferta nueva localhost:3000/api/offers/  {body}
+router.post('/', isAuthenticate, OfferController.create)
+//DELETE Borrar una oferta localhost:3000/api/offers/XXXX  
+router.delete('/:id',isAuthenticate, OfferController.delete)
+//PUT modificar una oferta localhost:3000/api/offers/XXXX  {body}
+router.put('/:id',isAuthenticate, OfferController.update)   
 
 // Calificamos una oferta x   {body}
-router.post('/:id/rate/',OffertController.rate)  
+router.post('/:id/rate/',isAuthenticate,OfferController.rate)  
 // Vemos que calificación (total) se le ha data a una oferta X
-router.get('/:id/rate/', OffertController.getRate)
+router.get('/:id/rate/', isAuthenticate, OfferController.getRate)
 
- */
+
 
 export default router
